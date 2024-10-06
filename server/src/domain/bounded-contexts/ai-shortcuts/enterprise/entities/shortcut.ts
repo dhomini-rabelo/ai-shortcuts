a7a1b69fc0e28b2741c99376f5c05826dfeccb01
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-10-06 19:53:51 -0300

    feat: add shortcut entity

---
+++ server/src/domain/bounded-contexts/ai-shortcuts/enterprise/entities/shortcut.ts
@@ -0,0 +1,18 @@
+import { Entity } from "@/domain/core/entities/base"
+import { ID } from "@/domain/core/entities/id"
+
+export interface ShortcutProps {
+  text: string
+  isPinned: boolean
+  folderId: ID
+}
+
+export class Shortcut extends Entity<ShortcutProps> {
+  static create(props: ShortcutProps) {
+    return new Shortcut(props)
+  }
+
+  static reference(id: ID, props: ShortcutProps) {
+    return new Shortcut(props, id)
+  }
+}
