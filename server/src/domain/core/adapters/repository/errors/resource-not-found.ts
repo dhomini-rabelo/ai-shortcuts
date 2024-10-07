18de67927cb8bfebd62036fd5bde0310be61ca4a
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-10-06 21:00:46 -0300

    style[ResourceNotFoundError]: update typing

--- server/src/domain/core/adapters/repository/errors/resource-not-found.ts
+++ server/src/domain/core/adapters/repository/errors/resource-not-found.ts
@@ -1,12 +1,11 @@
 import { Entity } from '@/domain/core/entities/base'
-import { EmptyRecord } from '@typing/simple'
 
 import { DatabaseError } from './_base'
 
 export class ResourceNotFoundError extends DatabaseError {
   public readonly type = 'resource-not-found'
 
-  constructor(public readonly entity: Entity<EmptyRecord>) {
+  constructor(public readonly entity: Entity) {
     super()
   }
 }
