58087eedc602590c454df7fe794c1dd8e8bfd3ff
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-10-12 16:31:39 -0300

    style[Factory]: minor

--- server/tests/types/factory.ts
+++ server/tests/types/factory.ts
@@ -1,4 +1,4 @@
-import { Entity } from "@/domain/core/entities/base"
+import { Entity } from '@/domain/core/entities/base'
 
 export type Factory<EntityClass extends Entity> = {
   create: (data: Partial<EntityClass['props']>) => Promise<Entity>
