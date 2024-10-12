05111bb52123e11ef33e1a6157b36f6de3fb260d
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-10-12 16:31:05 -0300

    refactor: move OverWrite typing

--- server/src/domain/bounded-contexts/ai-shortcuts/application/use-cases/folder/create.ts
+++ server/src/domain/bounded-contexts/ai-shortcuts/application/use-cases/folder/create.ts
@@ -1,10 +1,9 @@
 import { ID } from '@/domain/core/entities/id'
 import { UseCase } from '@/domain/core/use-cases/base'
+import { OverWrite } from '@tests/types/utils'
 
 import { FolderProps } from '../../../enterprise/entities/folder'
 import { FolderRepository } from '../../repositories/folder'
-
-type OverWrite<T, U> = Pick<T, Exclude<keyof T, keyof U>> & U
 
 type Payload = OverWrite<
   FolderProps,
